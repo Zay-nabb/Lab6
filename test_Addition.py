411a548aa132529af2cb85fa8e90e6c0603e1573
Zaynab <156392721+Zay-nabb@users.noreply.github.com>
2024-11-16 17:54:01 +0200

    Update test_Addition.py

--- test_Addition.py
+++ test_Addition.py
@@ -2,13 +2,13 @@ import unittest
 from Addition import Addition
 class TC(unittest.TestCase):
   def test_Add(self):
-    res = Addition.add(1,1)
+    res = Addition.Add(1,1)
     self.assertEqual(res, 2)
     
-    res = Addition.add(-5,5)
+    res = Addition.Add(-5,5)
     self.assertEqual(res, 0)
     
-    res = Addition.add(-7,-3)
+    res = Addition.Add(-7,-3)
     self.assertEqual(res, -10)
 
 if __name__ == "__main__":
